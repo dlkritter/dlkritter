096ae17115589f204a7ac7e3c3d7694379da424f
dlkritter <100829534+dlkritter@users.noreply.github.com>
2022-12-13 10:27:56 +0200

    Azure core crawler (one crate with auth at a time)
    Why do in the UI what you can do with CLI?

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,35 @@
-use polars::prelude::CsvReader;
-use polars::prelude::SerReader;
-use polars::frame::DataFrame;
-use std::result::Result;
-extern crate odbc;
-extern crate env_logger;
-use odbc::*;
-use std::io;
-use std::error::Error;
-use odbc_safe::AutocommitOn;
+extern crate log;
+use azure_core::error::{ErrorKind, ResultExt, Result};
+use azure_storage::core::prelude::*;
+use azure_storage_blobs::prelude::*;
 
-fn connect() -> std::result::Result<(), DiagnosticRecord> {
-
-    let env = create_environment_v3().map_err(|e| e.unwrap())?;
-
-    let mut buffer = String::new();
-    println!("Please enter connection string: ");
-    io::stdin().read_line(&mut buffer).unwrap();
-
-    let conn = env.connect_with_connection_string(&buffer)?;
-    insert_encounters(&conn, &enc_df)
-}
-
-fn insert_encounters<'env>(conn: &Connection<'env, AutocommitOn>, enc_df: Polars::frame::DataFrame) -> Result<(), Error> {
-
-
-
-    let stmt = Statement::with_parent(conn)?.prepare(
-        "Insert into fhir.RustTest values (?,?,?",)?;
+#[tokio::main]
+async fn main() -> azure_core::Result<()> {
+    let storage_client = StorageAccountClient::new_sas_token(http_client.clone(), //Insert repo name and container SAS token here"")?;
     
-    let stmt = stmt.bind_parameter(1, &enc_df.column("class.code"));
-    let stmt = stmt.bind_parameter(2, &enc_df.column("class.system")); 
-    let stmt = stmt.bind_parameter(3, &enc_df.column("id"));          
+    let blob_service = storage_client.blob_service_client();
+    let container_client = storage_client.container_client("paraglcontainer");
 
+    let page = blob_service
+        .list_containers()
+        .into_stream()
+        .next()
+        .await
+        .expect("stream failed")?;
 
-    match stmt.execute()? {
-        Data(mut stmt) => {
-            let cols = stmt.num_result_cols()?;
-            while let Some(mut cursor) = stmt.fetch()? {
-                for i in 1..(cols + 1) {
-                    match cursor.get_data::<&str>(i as u16)? {
-                        Some(val) => print!(" {}", val),
-                        None => print!(" NULL"),
-                    }
-                }
-                println!("");
-            }
-        }
-        NoData(_) => println!("Query executed, no data returned"),
+    let page = container_client
+        .list_blobs()
+        .max_results(NonZeroU32::new(3u32).unwrap())
+        .into_stream()
+        .next()
+        .await
+        .expect("stream failed")?;
+
+    println!("List blob returned {} blobs.", page.blobs.blobs.len());
+    for cont in page.blobs.blobs.iter() {
+        println!("\t{}\t{} bytes", cont.name, cont.properties.content_length);
     }
 
     Ok(())
-}
-
-fn main() {
-
-    env_logger::init();
-
-    let pt_df = CsvReader::from_path("ptflat.csv").unwrap()
-    .infer_schema(None)
-    .has_header(true)
-    .finish()
-    .unwrap();
-
-    let enc_df = CsvReader::from_path("encflat.csv").unwrap()
-        .infer_schema(None)
-        .has_header(true)
-        .finish()
-        .unwrap();
-
-        match connect() {
-            Ok(()) => println!("Success"),
-            Err(diag) => println!("Error: {}", diag),
-        };
-    
-    
-
-    println!("{:?} {:?}{:?}{:?}", enc_df, pt_df, enc_df.schema(), pt_df.schema());
 
 }
